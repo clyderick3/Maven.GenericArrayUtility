38a9180bb84b7e045b478d525551814c3cc9a7b4
Clyde Broderick <clyderick3@gmail.com>
2022-04-07 20:24:02 -0400

    Used generic to make methods. Passed numOfOcc tests.

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,32 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T>{
+private T[] inputArray;
+
+    public ArrayUtility(T[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        int count = 0;
+        for (int i = 0; i < inputArray.length; i++){
+            if (inputArray[i] == valueToEvaluate) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
 }
